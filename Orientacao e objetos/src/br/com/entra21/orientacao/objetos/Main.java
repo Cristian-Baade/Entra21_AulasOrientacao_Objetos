cd506732eb467dc535b8091672431cb1975075b9
Cristian-Baade <Schauffertcristian@gmail.com>
2022-05-20 22:18:42 -0300

    Orientação a objetos

--- Orientacao e objetos/src/br/com/entra21/orientacao/objetos/Main.java
+++ Orientacao e objetos/src/br/com/entra21/orientacao/objetos/Main.java
@@ -3,47 +3,67 @@ package br.com.entra21.orientacao.objetos;
 import java.util.Scanner;
 
 import br.com.entra21.orientacao.objetos.principal.classes.Aluno;
+import br.com.entra21.orientacao.objetos.principal.classes.Professor;
 
 public class Main {
 
-	static Scanner entrada = new Scanner(System.in);
+		// o objeto da classe Scanner esta na variavel de entrada, então é um obj
+		//porem o System.in é uma chamada static pq
+		//para acessar o in do System nao precisei dar new
+		static Scanner entrada = new Scanner(System.in);
+		static Scanner entrada2 = new Scanner(System.in);
+		public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			
+			Professor professorJava = new Professor();
+			 
+			Professor professoraIngles = new Professor("Isabelle", (byte) 30);
 
-		Aluno alunoTeste = new Aluno();
+			System.out.println("Nome dela = "+professoraIngles.getNome());
+			
+			professoraIngles.setNome("Isabelle 2");
 
-		Aluno outroAluno = new Aluno();
+			System.out.println("agora o nome  dela = "+professoraIngles.getNome());
+			System.out.println("A idade dela é "+professoraIngles.getIdade());
+			
+			System.out.println("Os professoes trabalham na "+Professor.instituicao);
 
-		Aluno alunoObjeto;
+			Aluno alunoTeste= new Aluno(); 
+			
+			Aluno outroAluno= new Aluno();
+			
+			Aluno alunoNovato=new Aluno();
+			
+			Aluno alunoObjeto ;
+			
+			alunoObjeto= new Aluno();
+			
+			alunoObjeto.idade=18;
+			
+			 
+			alunoTeste.nome="Rubem";
+			alunoTeste.idade=33;
+			
+			outroAluno.idade=18;
+			outroAluno.nome="Visitante";
 
-		alunoObjeto = new Aluno();
+			alunoTeste.responderChamada();
+			alunoTeste.responderChamada();
+			alunoTeste.responderChamada();
+			alunoTeste.responderChamada();
+			
+			/*
+			 * System.out.println("Qual o nome do aluno novato?");
+			 * alunoNovato.nome=entrada.next();
+			 * 
+			 * System.out.println("Qual a idade do "+alunoNovato.nome+"?");
+			 * alunoNovato.idade=entrada.nextByte();
+			 * 
+			 * System.out.println("Bem vindo "+alunoNovato.nome);
+			 */
+			
+			
 
-		alunoObjeto.idade = 18;
-
-		alunoTeste.nome = "Rubem";
-		alunoTeste.idade = 33;
-
-		Aluno alunoNovato = new Aluno();
-
-		outroAluno.idade = 18;
-		outroAluno.nome = "Visitante";
-
-		alunoTeste.responderChamada();
-		alunoTeste.responderChamada();
-		alunoTeste.responderChamada();
-		alunoTeste.responderChamada();
-
-		/*
-		 * System.out.println("Qual o nome do aluno novato?");
-		 * alunoNovato.nome=entrada.next();
-		 * 
-		 * System.out.println("Qual a idade do "+alunoNovato.nome+ "?");
-		 * alunoNovato.idade=entrada.nextByte();
-		 * 
-		 * System.out.println("Bem vindo "+alunoNovato.nome);
-		 */
+		}
 
 	}
-
-}
